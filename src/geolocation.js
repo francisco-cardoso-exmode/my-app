3cab22111a229a20c98fc6eb1ae3673f5020b134
francisco-cardoso-exmode <francisco.cardoso@expertmode.pt>
2022-04-21 17:49:53 +0100

    4

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,25 +1,8 @@
 import React, {useState, useCallback} from 'react';
 import { Geolocation } from '@capacitor/geolocation';
 
-export default function GeolocationPage() {
+const printCurrentPosition = async () => {
+  const coordinates = await Geolocation.getCurrentPosition();
 
-  const [loc, setLoc] = useState(null);
-
-  const getCurrentPosition = useCallback(async () => {
-    const coordinates = await Geolocation.getCurrentPosition();
-    setLoc(coordinates);
-  }, [coordinates]);
-
-  return (
-    <div>
-      <h1>Geolocation</h1>
-      <p>Your location is:</p>
-      <p>Latitude: {loc?.coords.latitude}</p>
-      <p>Longitude: {loc?.coords.longitude}</p>
-
-      <button onClick={getCurrentPosition}>
-        Get Current Location
-      </button>
-    </div>
-  );
-}
+  console.log('Current position:', coordinates);
+};
