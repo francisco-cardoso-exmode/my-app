a5cc6f6ff8ed6f6eadb947c471dd2f4b95b714de
francisco-cardoso-exmode <francisco.cardoso@expertmode.pt>
2022-04-21 17:57:48 +0100

    6

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,8 +1,19 @@
-import React, {useState, useCallback} from 'react';
+
 import { Geolocation } from '@capacitor/geolocation';
 
-const printCurrentPosition = async () => {
-  const coordinates = await Geolocation.getCurrentPosition();
+export default function GeolocationPage() {
 
-  console.log('Current position:', coordinates);
-};
+    const printCurrentPosition = async () => {
+        const coordinates = await Geolocation.getCurrentPosition();
+      
+        console.log('Current position:', coordinates);
+      };
+
+      printCurrentPosition ();
+
+
+}
+
+
+
+
